c0ee4c345dcbede66c2d1a9a0a2d16102ec3ddf5
spences10 <spences10apps@gmail.com>
2021-07-03 20:59:27 +0100

    feat: :bug: fix session user logic

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,6 @@
 <script context="module">
   export async function load({ page, session }) {
-    if (page.path === '/admin' && session.user === null) {
+    if (page.path === '/admin' && session.user === '') {
       return {
         status: 302,
         redirect: '/',
