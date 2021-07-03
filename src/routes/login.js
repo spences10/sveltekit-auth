25fbf9701e706df927cb2b168565873e3ba48890
spences10 <spences10apps@gmail.com>
2021-07-03 20:57:31 +0100

    feat: :art: use uuid for sessionId

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,10 +1,12 @@
 import { CLIENT_ID } from '$lib/environment-variables'
+import { v4 as uuid } from 'uuid'
 
 const target = 'https://github.com/login/oauth/authorize'
 const clilentId = CLIENT_ID
 
 export async function get() {
-  const sessionId = '3231'
+  const sessionId = uuid()
+
   return {
     status: 302,
     headers: {
