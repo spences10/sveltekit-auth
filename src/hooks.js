5df6945f8159e47bbe7a4884ee0053748b11c3f3
spences10 <spences10apps@gmail.com>
2021-07-03 15:39:38 +0100

    feat: :sparkles: add logout endpoint

--- src/hooks.js
+++ src/hooks.js
@@ -2,9 +2,9 @@ import cookie from 'cookie'
 
 export async function handle({ request, resolve }) {
   const cookies = cookie.parse(request.headers.cookie || '')
-  const response = await resolve(request)
 
   request.locals.user = cookies.user
+  const response = await resolve(request)
 
   response.headers['set-cookie'] = `user=${
     request.locals.user || ''
