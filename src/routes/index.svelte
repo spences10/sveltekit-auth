4a460d62bdf7a31ffb2bfffc1fbb150a2476f38d
spences10 <spences10apps@gmail.com>
2021-07-03 14:32:45 +0100

    feat: :sparkles: add login endpoint

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,3 +3,6 @@
   Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the
   documentation
 </p>
+<a href="/login">
+  <button>Login using GitHub</button>
+</a>
---
+++ src/routes/login.js
@@ -0,0 +1,8 @@
+import { CLIENT_ID } from '$lib/env-varss'
+
+const target = 'https://github.com/login/oauth/authorize'
+const clilentId = CLIENT_ID
+export async function get(req) {
+  const sessionId = '3231'
+  return { status: 302, headers: { location: `${target}?client_id=${clilentId}&state=${sessionId}` } }
+}
