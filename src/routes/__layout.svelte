968dc28ba453518b1d91a116861a2e6794dcd6af
spences10 <spences10apps@gmail.com>
2021-07-03 16:12:08 +0100

    feat: :sparkles: add admin route and check

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,3 +1,15 @@
+<script context="module">
+  export async function load({ page, session }) {
+    if (page.path === '/admin' && session.user === null) {
+      return {
+        status: 302,
+        redirect: '/',
+      }
+    }
+    return { props: {} }
+  }
+</script>
+
 <script>
   import '../app.postcss'
 </script>
