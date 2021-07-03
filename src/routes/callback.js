af78c7171d204514fa863252b8aa9a6ecf6c85e3
spences10 <spences10apps@gmail.com>
2021-07-03 15:09:14 +0100

    feat: :sparkles: get user details from GitHub

--- src/routes/callback.js
+++ src/routes/callback.js
@@ -2,15 +2,18 @@ import { CLIENT_ID, CLIENT_SECRET } from '$lib/environment-variables'
 import fetch from 'node-fetch'
 
 const tokenUrl = 'https://github.com/login/oauth/access_token'
+const userUrl = 'https://api.github.com/user'
 const clientId = CLIENT_ID
 const secret = CLIENT_SECRET
 
 export async function get(req) {
   const code = req.query.get('code')
+
   const token = await getToken(code)
+  const user = await getUser(token)
 
   return {
-    body: token,
+    body: JSON.stringify(user, null, 2),
   }
 }
 
@@ -29,4 +32,13 @@ function getToken(code) {
   })
     .then(response => response.json())
     .then(response => response.access_token)
+}
+
+function getUser(token) {
+  return fetch(userUrl, {
+    headers: {
+      Accept: 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  }).then(response => response.json())
 }
